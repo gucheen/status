843774b78398459659fee72d8d690b1a31e85353
gucheen <guchengf@gmail.com>
2024-12-27 10:48:52 +0800

    feat: split application codes

---
+++ src/app.js
@@ -0,0 +1,76 @@
+import cookies from '@fastify/cookie'
+import helmet from '@fastify/helmet'
+import FastifyStatic from '@fastify/static'
+import view from '@fastify/view'
+import art from 'art-template'
+import Fastify from 'fastify'
+import esso from 'fastify-esso'
+import fp from 'fastify-plugin'
+import path from 'node:path'
+import staticResourcesHash from './auto-hash.js'
+import lowdbDatabasePlugin from './database.js'
+import { loginRoutes } from './login.js'
+import { mastodonRoutes } from './mastodon.js'
+import { statusRoutes } from './post.js'
+import { indexPlugin } from './index-page.js'
+
+const config = {
+  logger: true,
+}
+
+function createApp() {
+  const app = Fastify(config)
+
+  app.register(helmet, {
+    contentSecurityPolicy: false,
+  })
+  app.register(esso({
+    secret: process.env.Secret,
+    token_prefix: null,
+  }))
+  app.register(FastifyStatic, {
+    root: path.join(process.cwd(), 'public'),
+    prefix: '/public/', // optional: default '/'
+  })
+  app.get('/favicon.ico', (req, reply) => {
+    reply.sendFile('favicon.ico')
+  })
+  app.get('/favicon.svg', (req, reply) => {
+    reply.sendFile('favicon.svg')
+  })
+
+  app.register(cookies)
+  app.register(view, {
+    engine: {
+      'art-template': art,
+    },
+  })
+
+  app.register(staticResourcesHash)
+  app.register(lowdbDatabasePlugin)
+  app.register(fp(indexPlugin))
+
+  app.setErrorHandler(function (error, req, reply) {
+    if (error.statusCode === 401) {
+      reply.redirect(`/login?from=${encodeURIComponent(req.url)}`)
+    } else {
+      reply.send(error)
+    }
+  })
+
+  app.setNotFoundHandler((req, reply) => {
+    reply.sendFile('404.html')
+  })
+
+  app.register(loginRoutes)
+
+  app.register(statusRoutes)
+
+  if (process.env.mastodon_server) {
+    app.register(mastodonRoutes)
+  }
+
+  return app
+}
+
+export default createApp
--- src/index.js
+++ src/index.js
@@ -1,70 +1,11 @@
-import cookies from '@fastify/cookie'
-import helmet from '@fastify/helmet'
-import FastifyStatic from '@fastify/static'
-import view from '@fastify/view'
-import art from 'art-template'
-import Fastify from 'fastify'
-import esso from 'fastify-esso'
-import fp from 'fastify-plugin'
-import path from 'node:path'
-import staticResourcesHash from './auto-hash.js'
-import lowdbDatabasePlugin from './database.js'
-import { loginRoutes } from './login.js'
-import { mastodonRoutes } from './mastodon.js'
-import { statusRoutes } from './post.js'
-import { indexPlugin } from './index-page.js'
+import createApp from './app.js'
 
-const config = {
-  logger: true,
-}
-const fastify = Fastify(config)
+const app = createApp()
 
-fastify.register(helmet, {
-  contentSecurityPolicy: false,
-})
-fastify.register(esso({
-  secret: process.env.Secret,
-  token_prefix: null,
-}))
-fastify.register(FastifyStatic, {
-  root: path.join(process.cwd(), 'public'),
-  prefix: '/public/', // optional: default '/'
-})
-fastify.register(cookies)
-fastify.register(view, {
-  engine: {
-    'art-template': art,
-  },
-})
-
-fastify.register(staticResourcesHash)
-fastify.register(lowdbDatabasePlugin)
-fastify.register(fp(indexPlugin))
-
-fastify.setErrorHandler(function (error, req, reply) {
-  if (error.statusCode === 401) {
-    reply.redirect(`/login?from=${encodeURIComponent(req.url)}`)
-  } else {
-    reply.send(error)
-  }
-})
-
-fastify.setNotFoundHandler((req, reply) => {
-  reply.sendFile('404.html')
-})
-
-fastify.register(loginRoutes)
-
-fastify.register(statusRoutes)
-
-if (process.env.mastodon_server) {
-  fastify.register(mastodonRoutes)
-}
-
-fastify.listen({ host: '0.0.0.0', port: 3000 }, async (err) => {
+app.listen({ host: '0.0.0.0', port: 3000 }, async (err) => {
   if (err) {
-    fastify.log.error(err)
+    app.log.error(err)
     process.exit(1)
   }
-  fastify.buildIndex()
+  app.buildIndex()
 })
